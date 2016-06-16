b840b6c357c0eb2dd072afffa04b1ed6bd9c91d1
Phelps <phelpsmemo@gmail.com>
2016-06-16 13:16:55 -0300

    Update power_tree.ps1
    Adicionando informações importantes

--- power_tree.ps1
+++ power_tree.ps1
@@ -1,4 +1,7 @@
+#Script para gerar estrutura de permissão de todas as pastas de um determinado local
+#Importante para executar o script, voce antes deve liberar a execução de script no seu S.O
+
 echo "Gerando estrutura de compartilhamento das pastas"
 pause
-cd PATH
+cd PATH #Insira o caminho a ser analisado
 Get-Acl .\* | Format-List >> tree_folder.txt
